a3430ea5feb627274a4f28a3a0fa6382b60e5876
Jaydeep <300934575@PR-A315-14>
2017-08-17 11:12:56 -0400

    Added private instance variables and public properties.

--- COMP123-S2017-FinalExam-300934575/ScoreBoard.cs
+++ COMP123-S2017-FinalExam-300934575/ScoreBoard.cs
@@ -17,6 +17,78 @@ namespace COMP123_S2017_FinalExam_300934575
      */
     class ScoreBoard
     {
+        //PRIVATE INSTANCE VARIABLES
+        private TextBox _finalScoreTextBox;
+
+        private int _score;
+
+        private TextBox _scoreTextBox;
+
+        private int _time;
+
+        private TextBox _timeTextBox;
+
+        //PUBLIC PROPERTIES
+
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                this._score = value;
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+            }
+        }
+
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                this._time = value;
+            }
+        }
+
+        public TextBox TimerTextBox
+        {
+            get
+            {
+                return this._timeTextBox;
+            }
+            set
+            {
+                this._timeTextBox = value;
+            }
+        }
+
         
     }
 }
